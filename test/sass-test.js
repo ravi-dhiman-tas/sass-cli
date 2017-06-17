c56903ea114db82cdf41c36bdb0ab76c7192029f
ravid7000 <ravi@technoarchsoftwares.com>
2017-06-17 13:30:44 +0530

    Test case bug fixed

--- test/sass-test.js
+++ test/sass-test.js
@@ -73,7 +73,7 @@ describe('sass(1)', function () {
             var contents = fs.readFileSync(file, 'utf8')
             assert.equal(contents, '{\n' +
                 '  "name": "sass(1)-(no-args)",\n' +
-                '  "cli_version": "0.0.1",\n' +
+                '  "cli_version": "0.1.1",\n' +
                 '  "path": ".",\n' +
                 '  "type": "scss"\n' +
                 '}\n')
